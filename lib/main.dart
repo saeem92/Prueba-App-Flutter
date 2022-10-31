936aee1cd11c474c776e04728aa5ed36f95e5b5d
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-11-01 01:51:49 +0530

    raised buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,34 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp()); // This runapp function builds the main widget and calls build() on it.
 // }
 
-void main() => runApp(MyApp()); // Its same as above => is replacing curly brackets. They are used for functions with only 1 implementation.
+void main() => runApp(
+    MyApp()); // Its same as above => is replacing curly brackets. They are used for functions with only 1 implementation.
 
 class MyApp extends StatelessWidget {
   // extends means that this is inheriting from some other class
   @override
-  Widget build(BuildContext context) { // Build method returns the widgets should be rendered onto the screen
-
+  Widget build(BuildContext context) {
+    // Build method returns the widgets should be rendered onto the screen
+    var questions = ['What\'s your favorite color?', 
+    'What\'s your favorite animal?',
+    
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold( // scaffold is a widget in flutter that gives a basic design strucutre and color scheme and gives UI that looks more like a mobile app page.
-      appBar: AppBar(title: Text('PruebaApp'),),
-      body: Text('This is a default text!'),
+      home: Scaffold(
+        // scaffold is a widget in flutter that gives a basic design strucutre and color scheme and gives UI that looks more like a mobile app page.
+        appBar: AppBar(
+          title: Text('PruebaApp'),
+        ),
+        body: Column(children: [ 
+          Text('The question!'),
+          RaisedButton(child: Text('Answer 1'), onPressed: null,),
+          RaisedButton(child: Text('Answer 2'), onPressed: null,),
+          RaisedButton(child: Text('Answer 3'), onPressed: null,),
+        ],
+      ),
       ),
     ); // home is core widget which flutter will bring onto the screen
   }
 }
+
